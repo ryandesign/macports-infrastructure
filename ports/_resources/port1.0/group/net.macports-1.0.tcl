32ff86a3e7139de344bcfac44ce0206f9925c301
Ryan Schmidt <git@ryandesign.com>
2016-07-14 02:49:34 -0500

    net.macports-1.0.tcl: new portgroup

---
+++ ports/_resources/port1.0/group/net.macports-1.0.tcl
@@ -0,0 +1,26 @@
+# -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
+
+default categories          {sysutils}
+default platforms           {darwin}
+default license             {BSD}
+default maintainers         {portmgr}
+default supported_archs     {noarch}
+default homepage            {https://www.macports.org}
+default archive_sites       {}
+default master_sites        {}
+default distfiles           {}
+
+use_configure               no
+build {}
+pre-destroot {
+    xinstall -d ${destroot}/Library/LaunchDaemons
+}
+destroot {}
+post-destroot {
+    foreach f [glob -nocomplain -tails -directory ${destroot}/Library/LaunchDaemons net.macports.*.plist] {
+        ln -s ${f} ${destroot}/Library/LaunchDaemons/org.macports.${f}
+    }
+}
+default destroot.keepdirs   {${destroot}${prefix}/share}
+
+default livecheck.type      {none}
