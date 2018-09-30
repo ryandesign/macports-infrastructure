d81421796bb20bdfa0566bc8ac7b75df9cf8bd35
Ryan Schmidt <ryandesign@macports.org>
2018-09-30 05:08:27 -0500

    Fix plists for MacPorts 2.5

--- ports/_resources/port1.0/group/net.macports-1.0.tcl
+++ ports/_resources/port1.0/group/net.macports-1.0.tcl
@@ -9,18 +9,34 @@ default homepage            {https://www.macports.org}
 default archive_sites       {}
 default master_sites        {}
 default distfiles           {}
+default startupitem.uniquename \
+                            {${startupitem.name}}
 
 use_configure               no
+
 build {}
+
 pre-destroot {
     xinstall -d ${destroot}/Library/${startupitem.location}
 }
+
 destroot {}
-post-destroot {
-    foreach f [glob -nocomplain -tails -directory ${destroot}/Library/${startupitem.location} net.macports.*.plist] {
-        ln -s ${f} ${destroot}/Library/${startupitem.location}/org.macports.${f}
+
+# Add this post-destroot after any the port might declare, since we want to
+# find any plists they might have installed.
+port::register_callback net.macports.add_post_destroot
+proc net.macports.add_post_destroot {} {
+    post-destroot {
+        foreach f [glob -nocomplain -tails -directory ${destroot} Library/${startupitem.location}/net.macports.*.plist] {
+            set f /${f}
+            set uniquename [file rootname [file tail ${f}]]
+            xinstall -d ${destroot}${prefix}/etc/${startupitem.location}/${uniquename}
+            # Technically this is only needed if ${startupitem.install} is no.
+            ln -s ${f} ${destroot}${prefix}/etc/${startupitem.location}/${uniquename}/${uniquename}.plist
+        }
     }
 }
+
 default destroot.keepdirs   {${destroot}${prefix}/share}
 
 default livecheck.type      {none}
